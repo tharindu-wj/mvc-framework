09bd1bb4f66a63b734474051e27791205f1960cf
Tharindu <tharinduw@eight25media.com>
2018-06-19 09:37:56 +0530

    add boostrap class

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -5,3 +5,29 @@
  * Date: 6/18/18
  * Time: 5:13 PM
  */
+
+class Bootstrap{
+    private $controller;
+    private $action;
+    private $request;
+
+    public function __construct($request)
+    {
+        $this->request = $request;
+
+        if($this->request['controller'] == ""){
+            $this->controller = 'home';
+        }
+        else{
+            $this->controller = $this->request['controller'];
+        }
+
+        if($this->request['action'] == ""){
+            $this->action = 'index';
+        }
+        else{
+            $this->action = $this->request['action'];
+        }
+        echo $this->controller;
+    }
+}
--- index.php
+++ index.php
@@ -5,3 +5,9 @@
  * Date: 6/18/18
  * Time: 5:09 PM
  */
+
+require 'config.php';
+
+require 'classes/Bootstrap.php';
+
+$bootstrap = new Bootstrap($_GET);
