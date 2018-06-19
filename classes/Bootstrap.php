a76efb6e50214dcca27e808742138b1bbf56a27e
Tharindu <tharinduw@eight25media.com>
2018-06-19 15:51:31 +0530

    createcontroller class

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -30,4 +30,34 @@ class Bootstrap{
         }
         echo $this->controller;
     }
+
+
+    public function createController(){
+        //Check Class
+        if(class_exists($this->controller)){
+            $parents = class_parents($this->controller);
+            //Check Extend
+            if(in_array("Controller", $parents)){
+
+                if(method_exists($this->controller, $this->action)){
+                    return new $this->controller($this->action, $this->request);
+                }
+                else{
+                    //Method Does Not Exist
+                    echo '<h1>Method does not exist</h1>';
+                    return;
+                }
+            }
+            else{
+                //Base Controller Does Not Exists
+                echo '<h1>Base controller not found</h1>';
+                return;
+            }
+        }
+        else{
+            //Base Controller Does Not Exists
+            echo '<h1>Controller class does not exist</h1>';
+            return;
+        }
+    }
 }
