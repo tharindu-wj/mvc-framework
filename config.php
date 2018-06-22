80314e157c0c4366f9c677dd014e6ca6fb11cad2
Tharindu <tharinduw@eight25media.com>
2018-06-22 09:53:00 +0530

    configerations

--- config.php
+++ config.php
@@ -5,3 +5,14 @@
  * Date: 6/18/18
  * Time: 5:10 PM
  */
+
+//Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "asdf1234");
+define("DB_NAME", "mvc-framework");
+
+
+//Define URL
+define("ROOT_PATH", "/");
+define("ROOT_URL", "http://dev.mvc-framework.com/");
